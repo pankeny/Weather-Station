84066fa48d3f5397d29f7903c7b3c90a7007ecce
pankeny <konrad_malocha@hotmail.com>
2017-11-12 21:03:17 +0100

    Init rootlayout and add anchorpane to primarystage

--- src/pl/mkon/weatherstation/MainApp.java
+++ src/pl/mkon/weatherstation/MainApp.java
@@ -1,16 +1,61 @@
 package pl.mkon.weatherstation;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-
+	private Stage primaryStage;
+	private BorderPane rootLayout;
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Weather Conditions");
 		
+		initRootLayout();
+		displayWeatherConditions();
+	}
+	
+	public void initRootLayout()
+	{
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void displayWeatherConditions()
+	{
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/WeatherDisplay.fxml"));
+			AnchorPane WeatherDisplay = (AnchorPane) loader.load();
+			
+			rootLayout.setCenter(WeatherDisplay);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public Stage getPrimaryStage() {
+		return primaryStage;
 	}
 }
