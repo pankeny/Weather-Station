607835444baf1a9771f375f122f8061ed7040653
pankeny <konrad_malocha@hotmail.com>
2017-11-12 20:00:45 +0100

    Completed

--- src/pl/mkon/weatherstation/Main.java
+++ src/pl/mkon/weatherstation/Main.java
@@ -1,12 +1,21 @@
 package pl.mkon.weatherstation;
 
+import java.util.Scanner;
+
 import pl.mkon.weatherstation.temperatureread.TempSensorAccess;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
 		TempSensorAccess krakow = new TempSensorAccess("Krakow");
-		System.out.println(krakow.getTemp());
+		System.out.println("Przykładowy odczyt dla miasta Krakow\n" + krakow.showTemp());
+		
+		System.out.print("Podaj miasto dla którego chcesz odczytać bieżącą temperature: ");
+		TempSensorAccess city = new TempSensorAccess(in.nextLine());
+		System.out.println(city.showTemp());
+		
+		
 	}
 
 }
--- src/pl/mkon/weatherstation/temperatureread/TempSensorAccess.java
+++ src/pl/mkon/weatherstation/temperatureread/TempSensorAccess.java
@@ -1,19 +1,22 @@
 package pl.mkon.weatherstation.temperatureread;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.ParseException;
 
-import org.json.JSONObject;
-
-import netscape.javascript.JSException;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 public class TempSensorAccess {
 	
+	private String cityName;
 	private String url;
 	
 	public TempSensorAccess(String cityName)
 	{
+		this.cityName = cityName;
 		this.setURL(cityName);
 	}
 	
@@ -22,22 +25,29 @@ public class TempSensorAccess {
 		this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + name + "&APPID=af319cd969dff7d8c42768f6f0d8c979&units=metric";
 	}
 	
-	public String getTemp() throws Exception
+	public long getTemp()
 	{
-			
+
+		long temp = 0;
 		try {
 			JSONObject json = new JSONObject(readUrl(this.url));
-			return (String) json.get("main.temp");
-		}catch (JSException e) {
+			temp = (long) json.get("temp");
+			return temp;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}  catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		return null;
+		return -505;
 	}
 	
-	private static String readUrl(String urlString) throws Exception
+	public static JSONObject readUrl(String urlString) throws Exception
 	{
 		BufferedReader reader = null;
+		JSONObject json;
 		
 		try {
 			URL url = new URL(urlString);
@@ -48,13 +58,18 @@ public class TempSensorAccess {
 			while ( (read = reader.read(chars)) != -1) {
 				buffer.append(chars, 0, read);
 			}
-			
-			return buffer.toString();
+			json = (JSONObject) JSONValue.parseWithException(buffer.toString());
+			return (JSONObject) json.get("main");
 		} finally {
 			if (reader != null) {
 				reader.close();
 			}
 		}
+	}
+	
+	public String showTemp()
+	{
+		return new String("Temperatura w miescie " + this.cityName + " wynosi: " + this.getTemp() );
 	}
 	
 }
