910457ab12481c962b2249ea4987d16d13eb57b4
pankeny <konrad_malocha@hotmail.com>
2017-11-12 12:57:48 +0100

    init

---
+++ src/pl/mkon/weatherstation/Main.java
@@ -0,0 +1,10 @@
+package pl.mkon.weatherstation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/pl/mkon/weatherstation/temperatureread/TempAccess.java
@@ -0,0 +1,10 @@
+package pl.mkon.weatherstation.temperatureread;
+
+public class TempAccess {
+	String name;
+	
+	TempAccess(String name)
+	{
+		this.name = name;
+	}
+}
